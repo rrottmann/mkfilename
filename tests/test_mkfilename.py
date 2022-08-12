469ce0f96fa23cbcec94ab934355d80f70ded7f9
Reiner Rottmann <reiner@rottmann.it>
2022-08-12 16:11:24 +0100

    Added unittests

---
+++ tests/test_mkfilename.py
@@ -0,0 +1,27 @@
+import unittest
+import datetime
+from click.testing import CliRunner
+
+from mkfilename import mkfilename
+
+class MyTestCase(unittest.TestCase):
+    def test_converting_a_heading(self):
+        s = 'Python is a high-level, interpreted, general-purpose programming language.'
+        t = datetime.datetime.now().strftime("%y%m%d")
+        runner = CliRunner()
+        result = runner.invoke(mkfilename, ['--string', s, '--stdout'])
+        print(result)
+        self.assertEqual(result.stdout,
+                         f'{t}-python-is-a-high-level-interpreted-general-purpose-programming-language.md\n')
+    def test_converting_a_filename(self):
+        s = 'Python Logo.png'
+        t = datetime.datetime.now().strftime("%y%m%d")
+        runner = CliRunner()
+        result = runner.invoke(mkfilename, ['--string', s, '--stdout'])
+        print(result)
+        self.assertEqual(result.stdout,
+                         f'{t}-python-logo.png\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
